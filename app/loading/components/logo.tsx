00aaaa87c27386cad40a384d4a4b6df0998e5b5b
김수빈 <kimsubin@gimsubin-ui-MacBookPro.local>
2026-02-01 00:10:40 +0900

    First commit

---
+++ app/loading/components/logo.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+import logo from '@/public/DumMoDem_Logo.png'
+import Link from 'next/link'
+function Logo() {
+  return (
+    <section className="flex flex-row items-center justify-center gap-4">
+      {/* 클릭시 메인 페이지로 이동 */}
+      <Link href="/">
+        {/* 로고 이미지 영역 */}
+        <div className="flex flex-row items-center justify-center gap-4">
+          <Image src={logo} alt="logo" width={100} height={100} />
+          {/* 로고 텍스트 영역 */}
+          <p className="text-3xl font-bold "> DunMoDem</p>
+        </div>
+      </Link>
+    </section>
+  );
+}
+
+export default Logo;
