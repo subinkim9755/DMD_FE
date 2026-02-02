173c49b445e07ee338eb24d94cd3890617fca60a
김수빈 <kimsubin@gimsubin-ui-MacBookPro.local>
2026-02-02 22:18:23 +0900

    메인페이지 경로생

---
+++ app/Main/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function mainPage() {
+  return (
+    <div>main페이지 입니다.</div>
+  )
+}
+
+export default mainPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Header from "../components/Header/Header";
+import Link from "next/link";
 
 export default function Home() {
   return (
 
     <div>
-      <h1>Hello World</h1>
-      <div>바로안되는데 시발</div>
+<Link href="/Main">
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-md w-70 h-15">
+        <p className="text-3xl font-bold "> main페이지 이동</p>
+      </button>
+    </Link>
     </div>
   );
 }
