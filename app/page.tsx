81a97f97e0d16a31894bfbaf63e670b01fa07c91
김수빈 <kimsubin@gimsubin-ui-MacBookPro.local>
2026-02-03 23:58:01 +0900

    랜디에이지 브랜치 생성

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,13 @@ export default function LandingPage() {
   return (
     <section className="flex flex-row items-center justify-center overflow-visible w-full h-screen bg-white">
 
-      <div className="flex flex-col items-center justify-center gap-4 w-full h-full overflow-visible md:max-w-[740px] lg:max-w-[1400px] gap-10">
+      <div className="flex flex-col items-center justify-start mt-20 gap-4 w-full h-full overflow-visible md:max-w-[740px] lg:max-w-[1400px] gap-10">
         
         <D_F_Text />
+<div>
+  <p>당신의 데미지를 계산해 보세요</p>
+</div>
+
         <CircularText text=" Calculating damage" onHover="goBonkers" color="#dfdfdf" spinDuration={20}>
           <PlayButton href="/main" />
         </CircularText>
